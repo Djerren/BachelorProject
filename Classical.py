3f194813144a6fc63259da15e382c1588645d348
Jaron <jaron.has@upcmail.nl>
2022-01-26 18:01:03 +0100

    comments

--- Classical.py
+++ Classical.py
@@ -1,5 +1,7 @@
-# for converting to binary string: '{0:07b}'.format(12)
+# Classical.py consists of all the necessary function to calculate the probability of the best
+# classical strategy for LSSD with three parties and |X| = |A| = |B| = |C|.
 
+# these four functions represent each of the possible binary functions.
 def id_func(bit):
     return bit
 
@@ -12,31 +14,44 @@ def bot(bit):
 def top(bit):
     return 1
 
+
 def find_win_prob(f, g, h, subset):
     """
-    This function returns the winning probability of a strategy given by 3 binary functions
-    given a subset of tuples (or lists) of 4 bits. (uniform distribution on subset is assumed.)
+    This function returns the winning probability of a classical strategy given by 3 binary functions.
+    subset is a list of tuples, where each tuple is a possible combination of choices from the set X, A, B and C
+    and the probability distribution is assumed to be uniform over this subset.
     """
     win_prob = 0
+
+    # For each combination in subset, the game is won if all three guesses (determined by the three binary functions)
+    # are the same as the first element x. We count the number of combinations in the subset for which the game is won.
     for element in subset:
         if element[0] == f(element[1]) == g(element[2]) == h(element[3]):
             win_prob += 1
 
-    # Division does not necessarily need to happen here.
+    # The probability that the game is won given the subset and the strategy is given by the number of combinations
+    # in the subset for which the game is won divided by the number of combination in the subset, because uniform
+    # distribution is assumed.
     return win_prob/len(subset)
 
 
 
 def find_optimal_strategy(subset):
+    """
+    This function receives a game, defined by the subset and finds the winning probability of the best
+    deterministic strategy.
+    """
+
     max_win_prob = 0
     bin_funcs = [id_func, not_func, bot, top]
+
+    # We loop over all possible strategies and determine their win probability, if this probability
+    # exceeds the current best probability, we replace the best win probability by this one
     for f in bin_funcs:
         for g in bin_funcs:
             for h in bin_funcs:
                 win_prob = find_win_prob(f,g,h, subset)
 
-                # Currently working with floats, should not cause problems.
-                # Possible to switch to ints and do division at the end.
                 if win_prob > max_win_prob:
                     max_win_prob = win_prob
     return max_win_prob
