76227a833c48701655979e0cd7a0a76cf539cfc7
Jaron <jaron.has@upcmail.nl>
2022-03-09 19:55:05 +0100

    added different ways of "optimizing gap"

--- optimal_gap.py
+++ optimal_gap.py
@@ -1,6 +1,7 @@
 from pulp import LpMaximize, LpProblem, LpStatus, lpSum, LpVariable, PULP_CBC_CMD
 from fractions import Fraction
 
+
 def two_player_binary_LP_pulp(det_strats, ns_strats, set_to_equality):
     model = LpProblem(name="max_gap_2player_bin", sense=LpMaximize)
 
@@ -98,7 +99,7 @@ def three_player_binary_LP_pulp(det_strats, ns_strats, set_to_equality):
 
     return model.objective.value()
 
-def two_player_x3_LP_pulp(det_strats, ns_strats, set_to_equality, show=False):
+def two_player_x3_LP_pulp(det_strats, ns_strats, set_to_equality, show=False, count=0, maximize="gap", ratio_value = 0.5/0.4, bias_value = 0.5/0.2):
     model = LpProblem(name="max_gap_2player_bin", sense=LpMaximize)
 
     # the variables are named as follows: the variable corresponding to P(x,a,b)
@@ -136,10 +137,17 @@ def two_player_x3_LP_pulp(det_strats, ns_strats, set_to_equality, show=False):
     # This constraint makes sure that whenever deterministic strategies are strictly
     # better that non-deterministic strategies, the LP has no solution.
     # (without this constraint, the solution could be negative). (honestly might be unnecessary)
-    model += (max_ns - max_det >= 0)
+    if maximize == "gap":
+        model += (max_ns - max_det >= 0)
+    elif maximize == "ratio":
+        model += (max_ns - ratio_value * max_det >= 0)
+    elif maximize == "bias":
+        model += (3*max_ns - (bias_value * 3)*max_det + (bias_value - 1)>=0)
 
+    # objective function is not important for maximize="ratio", since we are only interested in whether there is a solution.
     obj_func = max_ns - max_det
     model += obj_func
+
     #print(model)
 
     model.solve(PULP_CBC_CMD(msg=0))
@@ -152,15 +160,34 @@ def two_player_x3_LP_pulp(det_strats, ns_strats, set_to_equality, show=False):
     """
     if model.status == 1:
         if show and model.objective.value() > 0:
-            print(f"status: {model.status}, {LpStatus[model.status]}, gap: {model.objective.value()}")
-            print(f"objective: {model.objective.value()}")
-            for var in model.variables():
-                print(f"{var.name}: {var.value()},", end=" ")
-            print("\n")
-
-        return model.objective.value()
+            if maximize == "gap":
+                print(f"status: {model.status}, {LpStatus[model.status]}, gap: {model.objective.value()}")
+                print(f"objective: {model.objective.value()}")
+                print("distribution:")
+                for var in model.variables():
+                    print(f"{var.name}: {var.value()},", end=" ")
+                strategy = ns_strats[set_to_equality]
+                print()
+                print("strategy:")
+                for x in range(3):
+                    for ab in range(4):
+                        print(f"{x}{ab}: {strategy[x*16 + ab]},", end=" ")
+                print(f"\nstrategy number: {set_to_equality}")
+                print("")
+                count += 1
+            elif maximize == "ratio":
+                print(f"solution found:")
+                for var in model.variables():
+                    print(f"{var.name}: {var.value()},", end=" ")
+                print("\n")
+            elif maximize == "bias":
+                print(f"solution found:")
+                for var in model.variables():
+                    print(f"{var.name}: {var.value()},", end=" ")
+                print("\n")
+        return model.objective.value(), count
     else:
-        return 0
+        return 0, count
 
 def max_gap_two_player_binary():
     det_strats = read_strategies_from_file("two_player_binary_det_strats.txt")
@@ -189,14 +216,15 @@ def max_gap_three_player_binary():
 def max_gap_two_player_x3():
     det_strats = read_strategies_from_file("two_player_x3_det_strats.txt")
     ns_strats = read_strategies_from_file("two_player_x3_ns_strats.txt")
-
+    gap_strategies = [2, 12, 34, 36, 41, 45, 133, 135, 137, 139, 279, 289, 303, 308, 319, 323, 328, 329, 334, 335, 463, 474, 497, 517]
     max_gap = 0
-    for i in range(len(ns_strats)):
-        gap = two_player_x3_LP_pulp(det_strats, ns_strats, i, True)
+    count = 0
+    for i in gap_strategies:
+        gap, count = two_player_x3_LP_pulp(det_strats, ns_strats, i, True, count, "bias", bias_value=(0.5/0.2)*(100))
         if gap > max_gap:
             max_gap = gap
 
-    return max_gap
+    return max_gap, count
 
 def read_strategies_from_file(file):
     strategies = []
